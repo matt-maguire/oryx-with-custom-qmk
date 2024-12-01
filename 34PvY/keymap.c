757dd979c73fc504a852c4f934b9aeb556121ae5
Matt Maguire <matthew.maguire@gmail.com>
2024-12-01 13:01:56 +1100

    use basic keycodes in key overrides

--- 34PvY/keymap.c
+++ 34PvY/keymap.c
@@ -168,13 +168,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 // Custom QMK here
 const key_override_t unds_key_override =
-    ko_make_with_layers(MOD_MASK_SHIFT, KC_QUOT, KC_UNDS, 1);
+    ko_make_with_layers(MOD_MASK_SHIFT, KC_QUOT, KC_MINS, 1);
 const key_override_t ques_key_override =
-    ko_make_with_layers(MOD_MASK_SHIFT, KC_COMM, KC_QUES, 1);
+    ko_make_with_layers(MOD_MASK_SHIFT, KC_COMM, KC_SLSH, 1);
 const key_override_t lt_key_override =
-    ko_make_with_layers(MOD_MASK_SHIFT, KC_SLSH, KC_LT, 1);
+    ko_make_with_layers(MOD_MASK_SHIFT, KC_SLSH, KC_COMM, 1);
 const key_override_t dquo_key_override =
-    ko_make_with_layers(MOD_MASK_SHIFT, KC_MINS, KC_DQUO, 1);
+    ko_make_with_layers(MOD_MASK_SHIFT, KC_MINS, KC_QUOT, 1);
 
 const key_override_t **key_overrides = (const key_override_t *[]){
 	&unds_key_override,
